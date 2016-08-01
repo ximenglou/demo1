3ea322bc146721d78c8b0728270095f9e6737ff5
ximenglou <ximenglou@foxmail.com>
2016-08-01 11:02:59 +0800

    test

---
+++ demo/src/main/java/Haha.java
@@ -0,0 +1,13 @@
+/**
+ * Created by liqiyu on 2016-08-01-10:55.
+ */
+public class Haha implements Jiekou{
+    int a;
+    int b;
+    String aa;
+    String bb;
+
+    public void show() {
+
+    }
+}
---
+++ demo/src/main/java/Jiekou.java
@@ -0,0 +1,6 @@
+/**
+ * Created by liqiyu on 2016-08-01-11:01.
+ */
+public interface Jiekou {
+    void show();
+}
